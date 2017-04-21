a12b47f2b6ffc8b7e0d518451e51107d07dbfce7
Martin <martin@ideaaab.com>
2017-04-21 13:17:10 +0200

    Commit inicial
    Libreria funcional

---
+++ servo.c
@@ -0,0 +1,205 @@
+/*
+ * Libreria para mover servos
+*/
+
+#include "servo.h"
+
+/*
+ * TIMER 1
+ * Interrumpe cada 5mS
+ * En cada interrupcion se pone en high el pin del servo correspondiente y se
+ * actualiza el CCP con el valor de dicho servo.
+ * Cuando trabajamos con menos de 4 servos los servos que no se usan no tienen
+ * CCP al que actualizar, por lo que se usa un valor cualquiera dentro del rango
+ */
+#int_TIMER1 //timer control de servo
+void TIMER1_isr(void) {
+//Configurado para 1 tick cada 1 uS
+short ServoOutOfRange = FALSE;
+	
+	set_timer1(CERO_TIMER1);	//reinicia el timer
+	
+	switch(ServoFrame){
+		case(0):
+#ifdef SERVO1_PIN
+			output_bit(SERVO1_PIN, Servo[0].enabled);
+#endif
+			break;
+		case(1):
+#ifdef SERVO2_PIN
+			output_bit(SERVO2_PIN, Servo[1].enabled);
+#else
+			ServoOutOfRange = TRUE;
+#endif
+			break;
+		case(2):
+#ifdef SERVO3_PIN
+			output_bit(SERVO3_PIN, Servo[2].enabled);
+#else
+			ServoOutOfRange = TRUE;
+#endif
+			break;
+		case(3):
+#ifdef SERVO4_PIN
+			output_bit(SERVO4_PIN, Servo[3].enabled);
+#else
+			ServoOutOfRange = TRUE;
+#endif
+			flagAumentarDuty = TRUE;
+			break;
+	}
+	
+	if(ServoOutOfRange == FALSE){
+		CCP_1 = CERO_TIMER1 + Servo[ServoFrame].pos;   //actualiza el CCP con el valor de posicion actual
+	}
+	else{
+		CCP_1 = CERO_TIMER1 + SERVO_POS_MAX;   //actualiza el CCP con un valor "cualquiera" de servo
+	}
+}
+
+/* CCP */
+#int_CCP1
+void CCP1_isr(void){
+	//apaga el servo correspondiente
+	switch(ServoFrame){
+		case(0):
+#ifdef SERVO1_PIN
+			output_low(SERVO1_PIN);
+#endif
+			ServoFrame = 1;
+			break;
+		case(1):
+#ifdef SERVO2_PIN
+			output_low(SERVO2_PIN);
+#endif
+			ServoFrame = 2;
+			break;
+		case(2):
+#ifdef SERVO3_PIN
+			output_low(SERVO3_PIN);
+#endif
+			ServoFrame = 3;
+			break;
+		case(3):
+#ifdef SERVO4_PIN
+			output_low(SERVO4_PIN);
+#endif
+			ServoFrame = 0;
+			break;
+	}
+}
+
+/* RUTINAS */
+
+/* 
+ * Configura el timer1 para que tenga ticks de 1uS
+ * Habilita las interrupciones
+*/ 
+void Servo_init(void){
+//timer1 config
+#IF getenv("CLOCK") == 4000000
+	setup_timer_1(T1_INTERNAL|T1_DIV_BY_1);		
+#ELIF getenv("CLOCK") == 8000000
+	setup_timer_1(T1_INTERNAL|T1_DIV_BY_2);
+#ELIF getenv("CLOCK") == 16000000
+	setup_timer_1(T1_INTERNAL|T1_DIV_BY_4);
+#ELIF getenv("CLOCK") == 32000000
+	setup_timer_1(T1_INTERNAL|T1_DIV_BY_8);
+#ELSE
+	#ERROR La velocidad del PIC debe ser de 4, 8, 16 o 32Mhz
+#ENDIF
+	
+	CCP_1 = Servo[0].min;			//le da el primer valor a CCP1 para que interrumpa cuando debe
+	setup_ccp1(CCP_COMPARE_INT);	//configura CCP
+	
+	enable_interrupts(INT_TIMER1);	//activa timer 1
+	enable_interrupts(INT_CCP1);	//activa CCP1
+	enable_interrupts(GLOBAL);		//enable global interrupt
+}
+
+/*
+ * Configura el servo
+ * -num: numero del servo que estamos configurando (entre 0 y NUM_SERVOS-1)
+ * -vel: velocidad a la que se mueve (de 0 a 7)
+ * -min: valor minimo para el servo (en uS) tambien llamado POSICION1
+ * -max: valor maximo para el servo (en uS)	tambien llamado POSICION2
+*/
+void Servo_Config(int num, int vel, long min, long max){
+	//de momento asumimos que el usuario ha configurado el TRIS de cada pin
+	Servo[num].vel = vel;	//establece velocidad
+	Servo[num].min = min;	//establece minimo
+	Servo[num].max = max;	//establece maximo
+
+	Servo[num].pos = min;	//establece posicion actual
+	Servo[num].fin = min;
+	Servo[num].contES = 0;
+	Servo[num].enabled = FALSE;
+	//Servo[num].enabled = TRUE;
+}
+
+/*
+ * ACTIVA O DESACTIVA EL ENVIO DE PULSOS AL SERVO
+ *	"num" dice a que servo va dirigido
+ *	"en" dice si lo activamos (true) o desactivamos (false)
+ */
+void Servo_Active(int num, short en){
+	Servo[num].contES = 0;
+	Servo[num].enabled = en;
+}
+
+/*
+ * POSICIONAMIENTO DE SERVOS
+ * "num" dice a que servo va dirigido
+ * "pos" dice la posicion del servo a la que queremos ir
+ * -no hace falta comprobar que estamos dentro de los limites de min y max ya que 
+ * esto se hace en Servo_Refresh_Pos()
+ */
+void Servo_Mover(int num, long pos){
+	Servo[num].fin = pos;
+	Servo[num].contES = 0;
+	Servo[num].enabled = TRUE;
+}
+
+/*
+ * ACTUALIZA POSICIONES DE SERVOS
+ * esta rutina hay que llamarla constantemente desde el main
+ * para que actualice los valores de los servos
+*/
+void Servo_Refresh_Pos(void){
+	int x;
+	
+	if(flagAumentarDuty == TRUE){
+		flagAumentarDuty = FALSE;
+		
+		//aumenta la posicion en los servos que estan activos
+		for(x=0;x<MAX_SERVOS;x++){
+			if(Servo[x].enabled == TRUE){
+				/* estamos incrementando pos */
+				if(Servo[x].pos < Servo[x].fin){
+					Servo[x].pos = Servo[x].pos + ServoVel[Servo[x].vel];
+					//si nos pasamos
+					if(Servo[x].pos > Servo[x].fin){
+						Servo[x].pos = Servo[x].fin;
+					}
+				}
+				/* estamos disminuyendo pos */
+				else if(Servo[x].pos > Servo[x].fin){
+					Servo[x].pos = Servo[x].pos - ServoVel[Servo[x].vel];
+					//si nos pasamos
+					if(Servo[x].pos < Servo[x].fin){
+						Servo[x].pos = Servo[x].fin;
+					}
+				}
+				/* hemos llegado al final*/
+				else{	//servo[x].pos == servo[x].fin
+					//solo usamos el contador si EnergySave esta activado
+					if(EnergySave == TRUE){
+						if(++Servo[x].contES == VUELTAS_ENERGY_SAVE){
+							Servo[x].enabled = FALSE;
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ servo.h
@@ -0,0 +1,255 @@
+/*******************************************************************************
+ * File:   servo.h
+ * Author: Martin
+ *
+ * Created on 30 de abril de 2016, 18:45
+ * 
+ * Libreria que permite controlar entre 1 y 4 servos:
+ * -Permite configurar posicion min y max para cada servo
+ * -Permite configurar una velocidad para cada servo
+ * 
+ *******************************************************************************
+ * INSTRUCCIONES
+ * 
+ * -Hay que crear un define con la cantidad de servos a usar:
+ * #define NUM_SERVOS	4
+ * 
+ * -A cada servo (que este en uso) hay que crearle un define con su pin
+ * correspondiente:
+ * #define SERVO1_PIN	PIN_A1
+ * #define SERVO2_PIN	PIN_A2
+ * #define SERVO3_PIN	PIN_A3
+ * #define SERVO4_PIN	PIN_A4
+ * 
+ * -Se puede configurar un minimo y un maximo en uS para el movimiento de los
+ * servos. Por defecto son 500 y 2500 respectivamente. Usar los siguientes
+ * defines:
+ * #define SERVO_POS_MIN		1000
+ * #define SERVO_POS_MAX		2000
+ * 
+ * -Se puede configurar una velocidad por defecto para el movimiento del servo,
+ * por defecto la velocidad usada es la mas rapida, si se quiere usar otra crear
+ * el siguiente define con un valor entre 0 y (SERVO_NUM_VEL - 1)
+ * #define SERVO_VEL_DEFAULT	3
+ * 
+ * -Se puede configurar la posicion 1 y 2 por defecto para el movimiento del
+ * servo, por defecto las posiciones son 700uS y 1700uS, si se quiere usar otra
+ * crear los siguiente define con valores entre SERVO_POS_MIN y SERVO_POS_MAX
+ * #define SERVO_POS1			1200	//posicion1 por defecto
+ * #define SERVO_POS2			1900	//posicion2 por defecto
+ * 
+ * -Cuando EnergySave = TRUE se deja de enviar la posicion al servo transcurrido
+ * cierto tiempo. En la mayoria de los servos esto se traduce en que el servo
+ * deja de posicionarse y se queda "libre" en su posicion actual. Esto ahora
+ * bateria, pero permite que el servo pierda su posicion por accion de una
+ * fuerza externa. Por defecto esta activada, pero se puede desactivar creando
+ * el siguiente define:
+ * #define ENERGY_SAVE_DEFAULT	FALSE
+ * 
+ * -Cuando EnergySave = TRUE y una vez que el servo llego a la posicion final
+ * comienza un temporizador que una vez finalizado deja de enviar señal al servo
+ * El tiempo por defecto es de 1000mS, pero se puede cambiar creando el
+ * siguiente define:
+ * #define TIEMPO_ENERGY_SAVE	500
+ * 
+ * -Se crea un array con los valores de cada servo. Cada servo necesita 10 bytes
+ * y los valores son los siguientes:
+ * Servo[num]
+ *			.pos: posicion actual del servo
+ *			.fin: posicion a la que queremos llegar
+ *			.min: posicion minima del servo
+ *			.max: posicion maxima del servo
+ *			.enabled: flag para saber si el servo se esta moviendo
+ *			.vel: velocidad del servo, del 1 al 8
+ *			.contES: variable para contar el energy save
+ * 
+ *******************************************************************************
+ *FUNCIONES
+ * 
+ * -void Servo_init(void)
+ *	Inicializa la libreria
+ * 
+ * -void Servo_Config(int num, int vel, long min, long max)
+ *	Configura el servo
+ *		num: numero del servo que estamos configurando (entre 0 y NUM_SERVOS-1)
+ *		vel: velocidad a la que se mueve (de 0 a 7)
+ *		min: valor minimo para el servo (en uS)
+ *		max: valor maximo para el servo (en uS)
+ * 
+ * -void Servo_Active(int num, short en)
+ *	Activa o desactiva el envio de pulsos al servo
+ *		num: dice a que servo va dirigido
+ *		en: dice si lo activamos (true) o desactivamos (false)
+ * 
+ * -void Servo_Mover(int num, long pos)
+ *	Cambia el servo de posicion
+ *		num: dice a que servo va dirigido
+ *		pos: dice la posicion del servo a la que queremos ir
+ * 
+ * -void Servo_Refresh_Pos(void)
+ *	Actualiza la posicion de los servos. Esta rutina hay que ponerla en el main
+ *	para llamarla constantemente desde el main para que actualice los valores de
+ *  los servos
+ *  
+ ******************************************************************************/
+
+#ifndef SERVO_H
+#define	SERVO_H
+
+/* COMPROBACION DE SERVOS DECLARADOS */
+//1 servo
+#if NUM_SERVOS == 1
+#ifndef SERVO1_PIN
+#ERROR No se ha declarado el servo 1
+#endif
+#endif
+
+//2 servos
+#if NUM_SERVOS == 2
+#ifndef SERVO1_PIN
+#ERROR No se ha declarado el servo 1
+#endif
+
+#ifndef SERVO2_PIN
+#ERROR No se ha declarado el servo 2
+#endif
+#endif
+
+//3 servos
+#if NUM_SERVOS == 3
+#ifndef SERVO1_PIN
+#ERROR No se ha declarado el servo 1
+#endif
+
+#ifndef SERVO2_PIN
+#ERROR No se ha declarado el servo 2
+#endif
+
+#ifndef SERVO3_PIN
+#ERROR No se ha declarado el servo 3
+#endif
+#endif
+
+//4 servos
+#if NUM_SERVOS == 4
+#ifndef SERVO1_PIN
+#ERROR No se ha declarado el servo 1
+#endif
+
+#ifndef SERVO2_PIN
+#ERROR No se ha declarado el servo 2
+#endif
+
+#ifndef SERVO3_PIN
+#ERROR No se ha declarado el servo 3
+#endif
+
+#ifndef SERVO4_PIN
+#ERROR No se ha declarado el servo 4
+#endif
+#endif
+
+/* --- CONSTANTES --- */
+//genericas
+#ifndef TRUE
+#define TRUE				1
+#endif
+
+#ifndef FALSE
+#define FALSE				0
+#endif
+
+#define MAX_SERVOS			4	//cuantos servos podemos manejar
+
+#if NUM_SERVOS > MAX_SERVOS
+#error "No se pueden controlar mas de 4 servos con esta libreria"
+#endif
+
+#define T_T1				20	//cada cuanto interrumpe Timer1 en mS
+
+//velocidades
+#define SERVO_NUM_VEL		8
+
+#define SERVO_VEL1			1
+#define SERVO_VEL2			2
+#define SERVO_VEL3			3
+#define SERVO_VEL4			4
+#define SERVO_VEL5			5
+#define SERVO_VEL6			10
+#define SERVO_VEL7			50
+#define SERVO_VEL8			100
+
+#ifndef SERVO_VEL_DEFAULT
+#define SERVO_VEL_DEFAULT	(SERVO_NUM_VEL - 1)
+#endif
+
+//posiciones
+#define SERVO_PERIOD		20000	//en uS
+
+#ifndef SERVO_POS_MIN
+#define SERVO_POS_MIN		500		//posicion minima del servo
+#endif
+
+#ifndef SERVO_POS_MAX
+#define SERVO_POS_MAX		2500	//posicion maxima del servo
+#endif
+
+#ifndef SERVO_POS1
+#define SERVO_POS1			700		//posicion1 por defecto
+#endif
+
+#ifndef SERVO_POS2
+#define SERVO_POS2			1700	//posicion2 por defecto
+#endif
+
+#define S_RANGE				(SERVO_POS_MAX - SERVO_POS_MIN)	//rango de posiciones
+#define RANGE_UNI			(S_RANGE / 255.0)				//cuantas unidades de posicion incrementa cada unidad de adc
+
+#define CERO_TIMER1			(65535 - (SERVO_PERIOD / MAX_SERVOS))	//para que reinicie cada 5mS
+
+#define SERVO_MIN			(CERO_TIMER1 + SERVO_POS_MIN)	//posicion minima del servo con respecto al timer 1
+#define SERVO_MAX			(CERO_TIMER1 + SERVO_POS_MAX)	//posicion maxima del servo con respecto al timer 1
+
+#define SERVO_DEF1			(CERO_TIMER1 + SERVO_POS1)	//posicion 1 por defecto con respecto al timer 1
+#define SERVO_DEF2			(CERO_TIMER1 + SERVO_POS2)	//posicion 2 por defecto con respecto al timer 1
+
+//energy save
+#ifndef ENERGY_SAVE_DEFAULT
+#define ENERGY_SAVE_DEFAULT	TRUE
+#endif
+
+#ifndef TIEMPO_ENERGY_SAVE
+#define TIEMPO_ENERGY_SAVE	1000	//mS que mantiene el servo en posicion despues de haber llegado
+#endif								//tiene que ser multiplo de 20, y max 5100mS
+
+#define VUELTAS_ENERGY_SAVE	(1000 * TIEMPO_ENERGY_SAVE / SERVO_PERIOD)
+/* ------------------ */
+
+typedef struct{
+	long pos;		//posicion actual del servo (2 bytes)
+	long fin;		//posicion a la que queremos llegar (2 bytes)
+	long min;		//posicion minima del servo (2 bytes)
+	long max;		//posicion maxima del servo (2 bytes)
+	int enabled:1;	//flag para saber si el servo se esta moviendo (1 bit)
+	int vel:3;		//velocidad del servo, 8 velocidades (3 bits)
+	int nul:4;		//de momento estos 4 bits no se usan
+	int contES;		//variable para contar el energy save (1 byte)
+}servo_t;
+
+/* VARIABLES GOBALES */
+short EnergySave = ENERGY_SAVE_DEFAULT;		//indica si los servos funcionan constantemente o solo durante un tiempo
+short flagAumentarDuty = FALSE;	//indica si hay que modificar el pulso de los servos
+
+int ServoFrame = 0;				//variable para saber que servo estamos controlando
+
+const int ServoVel[SERVO_NUM_VEL] = {SERVO_VEL1, SERVO_VEL2, SERVO_VEL3, SERVO_VEL4, SERVO_VEL5, SERVO_VEL6, SERVO_VEL7, SERVO_VEL8};
+servo_t Servo[NUM_SERVOS];		//crea la variable de servos
+
+/* PROTOTIPOS */
+void Servo_init(void);
+void Servo_Config(int num, int vel, long min, long max);
+void Servo_Active(int num, short en);
+void Servo_Mover(int num, long pos);
+void Servo_Refresh_Pos(void);
+
+#endif	/* SERVO_H */
